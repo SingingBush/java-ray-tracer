effbfaa6426f7cc710584c7c15a2169ab1857433
SingingBush <singingbush@hotmail.com>
2020-02-08 22:39:31 +0000

    tweak to Vector3D class

--- blas/src/main/java/ex02/blas/Vector3D.java
+++ blas/src/main/java/ex02/blas/Vector3D.java
@@ -12,9 +12,10 @@ public class Vector3D {
 	private double magnitude;
 	
 	// Constructor
-	public Vector3D(double[] position, double[] direction, double magnitude) throws Exception {
-		if (position.length != 3 || direction.length != 3)
-			throw new Exception ("position and direction vectors must be of length 3.");
+	public Vector3D(double[] position, double[] direction, double magnitude) throws IllegalArgumentException {
+		if (position.length != 3 || direction.length != 3) {
+			throw new IllegalArgumentException ("position and direction vectors must be of length 3.");
+		}
 		
 		this.position = position.clone();
 		this.direction = direction.clone();
